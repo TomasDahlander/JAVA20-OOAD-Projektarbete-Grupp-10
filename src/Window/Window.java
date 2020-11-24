02717a68a1d94aba93512da4935d7156c0947e89
Tomas Dahlander <tomas.dahlander@gmail.com>
2020-11-24 16:18:43 +0100

    New Window class with GridLayouts

--- src/Window/Window.java
+++ src/Window/Window.java
@@ -1,55 +1,90 @@
 package Window;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.plaf.basic.BasicBorders;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Created by Tomas Dahlander <br>
+ * Date: 2020-11-24 <br>
+ * Time: 15:08 <br>
+ * Project: JAVA20-OOAD-Projektarbete-Grupp-10 <br>
+ */
 public class Window extends JFrame {
 
-    JTable table = new JTable(8, 2);
-    JPanel panel = new JPanel(new GridLayout(1, 3));
-    JPanel tablePanel = new JPanel(new BorderLayout());
-    JLabel label1 = new JLabel("Omgång          Poäng");
-    JPanel buttonPanel = new JPanel();
-    JToggleButton[] dice;
-    Random rng = new Random();
+    // Paneler
+    JPanel eastPanel = new JPanel(new BorderLayout());
+    JLabel tableLabel = new JLabel(" Omgång      Poäng ");
+    JPanel buttonPanel = new JPanel(new GridLayout(1,3));
+    JLabel name = new JLabel("Namn: Orvar Karlsson");
+
+    // Knappar
+    JButton roll = new JButton("Kasta");
+    JButton save = new JButton("Spara highscore");
+    JButton show = new JButton("Visa highscore");
+
+    // ScoreBoard
+    JPanel scoreBoard = new JPanel(new GridLayout(1,2));
+    JPanel scoreBoardRounds = new JPanel(new GridLayout(8,1));
+    JPanel scoreBoardScores = new JPanel(new GridLayout(8,1));
+    List<JLabel> scoreLabels = new ArrayList<>();
+    List<JLabel> roundLabels = new ArrayList<>();
+
+    JToggleButton[] dices; // Ej instansierad
 
 
+    public Window(){
+        setUpJFrame();
+        setUpPanels();
+        setUpScoreBoard();
+    }
 
-    public Window() {
+    public void setUpPanels(){
+        this.add(eastPanel,BorderLayout.EAST);
+        scoreBoardRounds.setBackground(Color.WHITE);
+        scoreBoardScores.setBackground(Color.WHITE);
+        scoreBoard.add(scoreBoardRounds);
+        scoreBoard.add(scoreBoardScores);
+        eastPanel.add(scoreBoard, BorderLayout.CENTER);
+        eastPanel.add(tableLabel, BorderLayout.NORTH);
+        buttonPanel.add(roll);
+        buttonPanel.add(save);
+        buttonPanel.add(show);
+        buttonPanel.setPreferredSize(new Dimension(390,40));
+        this.add(buttonPanel,BorderLayout.SOUTH);
+        this.add(name, BorderLayout.NORTH);
+    }
+
+    public void setUpScoreBoard(){
+        for(int i = 0; i < 8; i++){
+            roundLabels.add(new JLabel(String.valueOf(i+1)));
+            roundLabels.get(i).setBorder(new EtchedBorder());
+            roundLabels.get(i).setHorizontalAlignment(SwingConstants.CENTER);
+            roundLabels.get(i).setFont(new Font("MonoSpaced", Font.BOLD, 18));
+            scoreBoardRounds.add(roundLabels.get(i));
+            if (i == 6) roundLabels.get(i).setText("Bonus");
+            if (i == 7) roundLabels.get(i).setText("Summa");
+        }
+
+        for(int i = 0; i < 8; i++){
+            scoreLabels.add(new JLabel("")); // Just for show. this should be blank
+            scoreLabels.get(i).setFont(new Font("MonoSpaced", Font.PLAIN, 20));
+            scoreLabels.get(i).setBorder(new EtchedBorder());
+            scoreLabels.get(i).setHorizontalAlignment(SwingConstants.CENTER);
+            scoreBoardScores.add(scoreLabels.get(i));
+        }
+
+    }
+
+    public void setUpJFrame(){
         this.setLayout(new BorderLayout());
-        table.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        table.setRowHeight(51);
-        table.setGridColor(Color.BLACK);
-
-        panel.add(new JButton("Kasta"));
-        panel.add(new JButton("Spara highscore"));
-        panel.add(new JButton("Se highscore"));
-        tablePanel.add(label1, BorderLayout.NORTH);
-        tablePanel.add(table, BorderLayout.CENTER);
-
-        dice = toggleButtons();
-
-        this.add(panel, BorderLayout.SOUTH);
-        this.add(tablePanel, BorderLayout.EAST);
-
         this.setSize(new Dimension(400, 480));
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setVisible(true);
-    }
-
-    public JToggleButton[] toggleButtons() {
-        JToggleButton[] out = new JToggleButton[5];
-
-        for (int i = 0; i < 5; i++) {
-            out[i] = new JToggleButton("" + rng.nextInt(5) + 1);
-
-        }
-        return out;
     }
 
     public static void main(String[] args) {
